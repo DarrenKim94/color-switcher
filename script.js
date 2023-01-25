16820918d9683711f006856f4970b9159a124db6
Darren Kim <dkim555@yahoo.com>
2023-01-25 12:23:20 -0800

    shorten code

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-function changeColor() {
-    let colorOption = document.getElementById('list').value;
-    if (colorOption === 'red') {
-        document.body.style.backgroundColor = 'red'
-    }
-    if (colorOption === 'green') {
-        document.body.style.backgroundColor = 'green'
-    }
-    if (colorOption === 'blue') {
-        document.body.style.backgroundColor = 'blue'
-    }
-    if (colorOption === 'purple') {
-        document.body.style.backgroundColor = 'purple'
-    }
-    if (colorOption === 'yellow') {
-        document.body.style.backgroundColor = 'yellow'
-    }
+function changeColor(val) {
+    const colors = document.getElementById('list');
+    document.body.style.backgroundColor = colors.options[colors.selectedIndex].value;
 }
