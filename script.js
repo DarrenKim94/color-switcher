aa98024d9224f97f11f8bf574bd577cd0f37cc1e
Darren Kim <dkim555@yahoo.com>
2023-01-25 12:24:24 -0800

    remove parameter

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function changeColor(val) {
+function changeColor() {
     const colors = document.getElementById('list');
     document.body.style.backgroundColor = colors.options[colors.selectedIndex].value;
 }
